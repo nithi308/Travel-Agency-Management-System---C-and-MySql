4d2c69f9f25c7a68778caf9677640d0462246e79
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-25 14:15:29 +0530

    Add files via upload

---
+++ Travel Agency Management System - CodeWithC/cmake-build-debug/CMakeFiles/Travel_Agency_Management_System___CodeWithC.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Nithi/Desktop/Travel Agency Management System - C++ and MySql/Travel Agency Management System - CodeWithC/main.cpp" "C:/Users/Nithi/Desktop/Travel Agency Management System - C++ and MySql/Travel Agency Management System - CodeWithC/cmake-build-debug/CMakeFiles/Travel_Agency_Management_System___CodeWithC.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Travel Agency Management System - CodeWithC/cmake-build-debug/CMakeFiles/Travel_Agency_Management_System___CodeWithC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Travel_Agency_Management_System___CodeWithC.dir/main.cpp.obj"
+  "Travel_Agency_Management_System___CodeWithC.exe"
+  "Travel_Agency_Management_System___CodeWithC.exe.manifest"
+  "Travel_Agency_Management_System___CodeWithC.pdb"
+  "libTravel_Agency_Management_System___CodeWithC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Travel_Agency_Management_System___CodeWithC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
